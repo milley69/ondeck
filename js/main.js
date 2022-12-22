dd0d2475d7b429f1ead998e8aec640636da94879
milley <69milley@gmail.com>
2022-12-22 14:19:15 +0500

    qq

--- js/main.js
+++ js/main.js
@@ -4,27 +4,44 @@ let nav = document.querySelector('nav')
 let main = document.querySelector('main')
 let footer = document.querySelector('footer')
 let navLogin = document.querySelector('.nav-login')
+let navLinks = document.querySelector('.nav-links')
 let burger = document.querySelector('.burger');
+let mainImg = document.querySelector('.main-img');
 
 if (window.screen.width <= 700) {
     burger = document.createElement('div');
     burger.className = 'burger'
     burger.innerHTML = '<img src="img/Menu.svg" alt="burger"/>'
     nav.append(burger)
+    // mainImg.remove()
+    burger.onclick = () => {
+        navLogin.classList.add('nav-login-active')
+        navLinks.classList.add('nav-links-active')
+    }
+    main.onclick = () => fade();
+    footer.onclick = () => fade();
 }
-burger.onclick = () => {
-    navLogin.classList.add('nav-login-active')
+if (window.screen.width <= 580) {
+    mainImg.remove()
+    burger.onclick = () => {
+        navLinks.classList.add('nav-links-active')
+    }
 }
-main.onclick = () => {
+
+function fade() {
     navLogin.classList.add('fade')
-    setTimeout(()=>{
-        navLogin.classList.remove('nav-login-active')
-        navLogin.classList.remove('fade')
-    },500)
+    navLinks.firstElementChild.classList.add('fade')
+    setTimeout(() => {
+            navLogin.classList.remove('nav-login-active')
+            navLogin.classList.remove('fade')
+            navLinks.firstElementChild.classList.remove('fade')
+            navLinks.classList.remove('nav-links-active')
+        },
+        500)
 }
-footer.onclick = () => {
-    navLogin.classList.remove('nav-login-active')
-}
+
+
+
 products.onmouseover = function () {
     card.style.display = 'block'
     products.firstElementChild.style.color = '#142e47'
